dfedf4b390192ba2b7ef08ddba0b37b26b560703
Sachith Gunasekara <sachithgunasekara@outlook.com>
2023-06-24 13:38:59 +0530

    Fixed Test Class in product-service
    Test containers dependency issue resolved. This package is not used in this codebase.

--- product-service/src/test/java/lk/ac/kln/stu/gunaseka_ps17050/productservice/ProductServiceApplicationTests.java
+++ product-service/src/test/java/lk/ac/kln/stu/gunaseka_ps17050/productservice/ProductServiceApplicationTests.java
@@ -2,12 +2,8 @@ package lk.ac.kln.stu.gunaseka_ps17050.productservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.testcontainers.containers.MongoDBContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
-@Testcontainers
 class ProductServiceApplicationTests {
 
 	@Test
