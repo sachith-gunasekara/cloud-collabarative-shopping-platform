4dd895e0a33e7158d14176ef3dc0fd5037e53399
Ubuntu <sachithgunasekara@outlook.com>
2023-06-29 21:50:09 +0000

    Allow running on random port

--- inventory-service/src/main/java/lk/ac/kln/stu/gunaseka_ps17050/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/lk/ac/kln/stu/gunaseka_ps17050/inventoryservice/InventoryServiceApplication.java
@@ -15,20 +15,4 @@ public class InventoryServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryServiceApplication.class, args);
 	}
-
-	@Bean
-	public CommandLineRunner loadData(InventoryRepository inventoryRepository){
-		return args -> {
-			Inventory item1 = new Inventory();
-			item1.setSkuCode("iphone_13_silver");
-			item1.setQuantity(50);
-
-			Inventory item2 = new Inventory();
-			item2.setSkuCode("iphone_13_sierra_blue");
-			item2.setQuantity(0);
-
-			inventoryRepository.save(item1);
-			inventoryRepository.save(item2);
-		};
-	}
 }
